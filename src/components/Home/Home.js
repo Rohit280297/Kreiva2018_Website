fbc56fc6e4ff9431e9698fec57253e4823f40fe7
Rohit <shamdasanirohit0@gmail.com>
2018-09-24 03:05:34 +0530

    Modified Logo and Added NavBar Icon

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,49 @@ import Water from '../../assets/images/water_2.png';
 import Logo from '../../assets/images/mainLogo.png';
 import Ship from '../../assets/images/kreiva-ui_ship_ship.png';
 import Calendar from '../../assets/images/event.svg';
+import MenuBar from '../../assets/images/menu.svg';
+import fbIcon from '../../assets/images/facebook.svg';
+import instaIcon from '../../assets/images/instagram.svg';
+import twitterIcon from '../../assets/images/twitter.svg';
+class Home extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      menuOpen:false
+    }
+  }  
 
-export default () => {
-  return (
-    <div className="Home container-fluid">
-        <img id="MainLogo" src={Logo} alt="Logo"/>
-        <img id="Water_Home"src={Water} alt="Water" />
-        <img id="Ship_Home"src={Ship} alt="Ship" />
-        <p id="Social_Home">Follow Us</p>
-        <p id="Date_Home"><img  id = "Calendar"src={Calendar} alt="Calendar"/> 27-28th October, 2018</p>
-    </div>
-  )
+  openMenuHandler = () =>{
+    this.setState({menuOpen:true},()=>console.log(this.state.menuOpen));
+  }
+  render(){
+    return (
+      <div className="Home container-fluid">
+          <img id="MainLogo" src={Logo} alt="Logo"/>
+          <img id="Sidebar" src={MenuBar} alt="Logo" onClick={this.openMenuHandler}/>
+          <img id="Water_Home"src={Water} alt="Water" />
+          <img id="Ship_Home"src={Ship} alt="Ship" />
+          <div className="container">
+
+          <div className="LinksSection">
+                <div className="col-lg-6 col-md-12 col-sm-12">
+                    <div  id="Social_Home" >
+                        Follow Us 
+                        <img id="fb" src={fbIcon} alt="facebook"/>
+                        <img id="insta" src={instaIcon} alt="instagram"/>
+                        <img id="twitter" src={twitterIcon} alt="twitter"/>
+                    </div>
+                </div>
+                <div className="col-lg-6 col-md-12 col-sm-12">
+                  <div  id="Date_Home">
+                      <img id = "Calendar"src={Calendar} alt="Calendar"/> 27-28th October, 2018
+                  </div>
+                </div>
+          </div>
+          </div> 
+      </div>
+    )
+  }
 }
+
+export default Home;
